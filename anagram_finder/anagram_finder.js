c7c2a47f639547f091fa2bc3c1075f4ad64e2df1
Alastair Smith <j.alastair.smith@outlook.com>
2021-06-16 15:49:09 +0100

    adds anagram finder method and passes first test

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,26 @@
 const AnagramFinder = function (word) {
-
+    this.word = word.split('')
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
+    foundAnagrams = []
+    otherWords.forEach(word => {
+        if(checkLettersInWord(word, this.word))
+        foundAnagrams.push(word)
+    }) 
+    return foundAnagrams
+}
 
+
+function checkLettersInWord(word, arrayOfLetters){
+    let counter = 0
+    arrayOfLetters.forEach(letter => {
+        if(word.includes(letter))
+        counter++
+    })
+    if (counter === word.length){
+        return true
+    }
 }
 
 module.exports = AnagramFinder;
